dd8e40c46d37c3267e31e106979a8deaeb0e4b48
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-10-21 14:03:47 -0400

    css border and flexbox styling for each poison

--- scripts/poison.js
+++ scripts/poison.js
@@ -4,22 +4,26 @@ export const poison = (poison) => {
     if(poison.fatal) {
         poison.fatal = "Yes";
         return `
+        <section class="poison">
         <h2>${poison.commonName}</h2>
         <ul>
             <li>Species - ${poison.species}</li>
             <li>Fatal - ${poison.fatal}</li>
             <li>Antidote - ${poison.antidote}</li>
         </ul>
+        </section>
         `
     } else {
         poison.fatal = "No";
         return `
+        <section class="poison">
         <h2>${poison.commonName}</h2>
         <ul>
             <li>Species - ${poison.species}</li>
             <li>Fatal - ${poison.fatal}</li>
             <li>Antidote - ${poison.antidote}</li>
         </ul>
+        </section>
         `
     }
 }
